1b79d2f12d7cf41bd26411e1a7a7140b9b4e6fa0
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-03-05 04:46:52 +0700

    add wiring subscribe course and fetch course for user

--- src/pages/course/list.tsx
+++ src/pages/course/list.tsx
@@ -6,6 +6,8 @@ import { Course } from "@/services/course-service";
 import CourseService from "@/services/course-service";
 import Navbar from "../../components/navbar";
 import SearchBar from "@/components/adminCourse/search";
+import { Grid } from "@mui/material";
+import { Pagination } from "@mui/material";
 
 
 export default function Courses() {
@@ -20,6 +22,7 @@ export default function Courses() {
     CourseService.getAll({
       page: page,
       limit: perPage,
+      title: search,
     })
       .then((response) => {
         setCourses(response.data.data);
@@ -29,8 +32,37 @@ export default function Courses() {
       .catch((error) => console.log(error));
   }, [page, perPage]);
 
-  
-  
+  const [difficulty, setDifficulty] = useState("All Difficulty");
+  const [selected, setSelected] = useState<number[] | undefined>(undefined);
+
+  const searchQuery = (search: string) => {
+    CourseService.getAll({
+      page: page,
+      limit: perPage,
+      title: search,
+      difficulty: difficulty != "All Difficulty" ? difficulty : undefined,
+      categoryId: selected,
+    })
+      .then((response) => {
+        setCourses(response.data.data);
+        setPage(1)
+        setLength(response.data.meta.totalPage * perPage);
+        setCount(response.data.meta.totalPage);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      searchQuery(search);
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn); 
+  }, [search, difficulty, selected]);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   return (
     <>
@@ -45,20 +77,30 @@ export default function Courses() {
             },
             {
               label: "Courses",
-              href: "/courses",
+              href: "/course/list",
             },
           ]}
         />
       </div>
       <div className="container mx-auto flex flex-wrap justify-center custom-Montserrat ">
         <div className="w-full md:w-1/5 px-4">
-          <Sidebar />
+          <Sidebar difficulty={difficulty} setDifficulty={setDifficulty} selected={selected} setSelected={setSelected} />
         </div>
         <div className="w-full md:w-4/5 px-4">
           <div className="flex justify-center">
             <SearchBar searchTerm={search} setSearchTerm={setSearch} />
           </div>
           <CourseCards courses={courses} />
+          <Grid container direction="row" justifyContent="center" marginTop={2}>
+            <Pagination
+              count={count}
+              size="large"
+              page={page}
+              variant="outlined"
+              shape="rounded"
+              onChange={handleChange}
+            />
+          </Grid>
         </div>
       </div>
     </>
