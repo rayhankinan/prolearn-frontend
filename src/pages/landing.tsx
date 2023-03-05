dca15995c817d88ab627346e5951f0cecfe9da8f
Aira Thalca <13520101@std.stei.itb.ac.id>
2023-03-05 14:25:21 +0700

    wiring landing page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,90 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CardMedia from "@mui/material/CardMedia";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import CourseService from "@/services/course-service";
+import { Course } from "@/services/course-service";
+import CategoryService from "@/services/category-service";
+import { Category } from "@/services/category-service";
 
-const courseList = [
-    {   id: 1,
-        title: "Javascript",},
-    {
-        id: 2,
-        title: "Python",
-    },
-    {
-        id: 3,
-        title: "Java",
-    },
-    {
-        id: 4,
-        title: "C++",
-    },
-    {
-        id: 5,
-        title: "C",
-    },
-    {
-        id: 6,
-        title: "C#",
-    },
-    {
-        id: 7,
-        title: "Ruby",
-    },
-    {
-        id: 8,
-        title: "PHP",
-    },
-    {
-        id: 9,
-        title: "Swift",
-    },
-]
+export default function CourseLanding() {
+    const [courses, setCourses] = useState<Course[]>([]);
+    const APINEMBAK = "/api/file"
+    useEffect(() => {
+        CourseService.getAllForVisitor({
+        page: 1,
+        limit: 7,
+        })
+        .then((response) => {
+            setCourses(response.data.data);
+        })
+        .catch((error) => console.log(error));
+    }, []);
 
-const courseDetail = [
-    {
-        id: 1,
-        title: "Javascript",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 2,
-        title: "Python",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 3,
-        title: "Java",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 4,
-        title: "C++",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 5,
-        title: "C",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 6,
-        title: "C#",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-    {
-        id: 7,
-        title: "Ruby",
-        img: "../stok.jpg",
-        quantity: 10,
-    },
-]
+    const [categories, setCategories] = useState<Category[]>([]);
+    useEffect(() => {
+      CategoryService.getAll()
+        .then((response) => {
+          setCategories(response.data.data);
+        })
+        .catch((error) => console.log(error));
+    }, []);
+    const [categorySelected, setCategorySelected] = useState<number[]>([]);
+    const handleCategoryClicked = (id: number) => {
+        setCategorySelected([id]);
+    }
 
-const CourseLanding: React.FC = () => {
+    const search = (categorySelected:number[]|undefined) => {
+        CourseService.getAllForVisitor({
+          page: 1,
+          limit: 7,
+          categoryId: categorySelected
+        })
+          .then((response) => {
+            setCourses(response.data.data);
+          })
+          .catch((error) => console.log(error));
+    };
+
+    useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+        search(categorySelected);
+    }, 500);
+    return () => clearTimeout(delayDebounceFn); 
+    }, [categorySelected]);
+
     return (
         <div>
             <Navbar />
@@ -93,8 +61,16 @@ const CourseLanding: React.FC = () => {
                     <h1 className="font-sans text-6xl font-bold ml-24"> Learn Everywhere Like a Professional</h1>
                     <p className="font-sans text-2xl ml-24 mt-10">| A learning system based on formalised teaching with the help of resources.</p>
                     <div className="flex flex-row justify-start ml-14 mt-10">
-                        <a href="#" className="inline-block w-auto h-16 text-xl px-4 py-5 leading-none border border-gray-300 shadow-xl text-black bg-white mt-4 lg:mt-0 mr-12 ml-10 hover:transition ease-in-out hover:scale-110 duration-300 ">Get Started</a>
-                        <a href="#" className="inline-block w-auto h-16 text-xl px-4 py-5 leading-none border border-gray-300 shadow-xl text-black bg-gray-400 mt-4 lg:mt-0 mr-5 hover:transition ease-in-out hover:scale-110 duration-300">Learn More</a>
+                        <a href="/auth/register" className="inline-block w-auto h-16 text-xl px-4 py-5 
+                        leading-none border border-gray-300 shadow-xl text-black bg-white 
+                        mt-4 lg:mt-0 mr-12 ml-10 hover:transition ease-in-out hover:scale-110 duration-300 ">
+                            Get Started
+                        </a>
+                        <a href="#" className="inline-block w-auto h-16 text-xl px-4 py-5 
+                        leading-none border border-gray-300 shadow-xl text-black bg-gray-400 
+                        mt-4 lg:mt-0 mr-5 hover:transition ease-in-out hover:scale-110 duration-300">
+                            Learn More
+                        </a>
                     </div>
                 </div>
                 <div className="flex justify-end ">
@@ -125,28 +101,43 @@ const CourseLanding: React.FC = () => {
             <div className="flex flex-col justify-between w-full pb-20 pt-16 ">
                 <div className="flex w-auto flex-col bg-gray-300 mr-20 ml-20 p-4 rounded-xl">
                     <ul className="flex w-full justify-center ">
-                        {courseList.map((course) => (
-                            <li className="flex-1 mr-2">
-                                <a className="text-center block py-2 px-4 hover:bg-gray-400 rounded-xl text-gray-800" href="#">{course.title}</a>
+                        {categories.map((category) => (
+                            <li key={category.title} className="flex-1 mr-2">
+                                <a className="text-center block py-2 px-4 hover:bg-gray-400 
+                                rounded-xl text-gray-800" onClick={() => handleCategoryClicked(category.id)}>
+                                    {category.title}
+                                </a>
                             </li>
                         ))}
                     </ul>
                 </div>
                 <div className="grid grid-cols-4 ml-10 mr-10">
-                    {courseDetail.map((course) => (
-                        <div className="flex flex-wrap mx-1 lg:mx-10 rounded-xl border hover:border-gray-400 overflow-hidden shadow-xl ml-20 mt-20 mr-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-                            <img className="bg-cover" src={course.img} alt="Course Logo" />
+                    {courses.map((course) => (
+                        <div key={course.id} className="flex flex-wrap mx-1 lg:mx-10 rounded-xl border 
+                        hover:border-gray-400 overflow-hidden shadow-xl ml-20 mt-20 mr-20 transition ease-in-out 
+                        delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                            <CardMedia
+                                component="img"
+                                image={
+                                    course.__thumbnail__
+                                    ? `${APINEMBAK}/${course.__thumbnail__.id}`
+                                    : "https://source.unsplash.com/random"
+                                }
+                                alt="random"
+                                sx={{ height: "300px", objectFit: "cover" }}
+                            />
                             <div className="w-full border-t-2"></div>
                             <div className="px-6 py-4 bg-white w-full">
                                 <div className="font-bold text-xl mb-2">{course.title}</div>
                                     <p className="text-gray-700 text-base">
-                                        {course.quantity} Courses
+                                        {Math.floor(Math.random() * 100)} Section
                                     </p>
                             </div>
                         </div>
                         
                     ))}
-                    <div className="flex flex-wrap mx-1 lg:mx-10 rounded-xl overflow-hidden shadow-lg ml-20 mt-20 mr-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                    <div className="flex flex-wrap mx-1 lg:mx-10 rounded-xl overflow-hidden shadow-lg ml-20 
+                    mt-20 mr-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                         <div className="w-full h-full flex flex-col bg-gray-600 justify-center text-center">
                             <h1 className="font-sans text-8xl font-bold text-white">. . .</h1>
                             <h2 className="font-sans text-4xl pt-10 pb-12 font-bold text-white">See More</h2>
@@ -169,5 +160,3 @@ const CourseLanding: React.FC = () => {
         </div>
     );
 };
-
-export default CourseLanding;
--- src/services/course-service.ts
+++ src/services/course-service.ts
@@ -20,6 +20,10 @@ class CourseService {
         return http.get("/course", { params });
     }
 
+    getAllForVisitor(params?: any) {
+        return http.get("/course/visitor", { params });
+    }
+
     getById(id: number) {
         return http.get(`/course/${id}`);
     }
