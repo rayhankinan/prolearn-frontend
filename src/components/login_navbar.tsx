51779febb8d602a73fda7628c3a9994b6d3f932d
Rizky Akbar Asmaran <16520368@std.stei.itb.ac.id>
2023-03-22 19:47:14 +0700

    fix: misstype

--- src/components/login_navbar.tsx
+++ src/components/login_navbar.tsx
@@ -40,7 +40,7 @@ const Login_navbar: React.FC = () => {
         </div>
         <div className="mr-10">
           <a
-            href="/auth/logout"
+            href="/auth/login"
             className="inline-block w-32 h-auto text-xl px-4 py-2 leading-none border
             rounded-xl text-black bg-red-400 mt-4 lg:mt-0 mr-5 text-center align-middle
             hover:transition ease-in-out hover:scale-110 duration-300"
